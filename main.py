84364a5c2c9510f2be5998c9a9c65b493b940263
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-12-07 13:45:03 +0200

    First working template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import random
 from datetime import datetime, timedelta
 
@@ -25,8 +25,15 @@ daily_chore_assignments = {"Jaanuk": None, "Peach": None}
 completed_chores = {"Jaanuk": 0, "Peach": 0}
 
 def choose_random_chores(person):
-    available_chores = list(list_of_chores - daily_chore_assignments["Jaanuk"] - daily_chore_assignments["Peach"])
+    available_chores = set(list_of_chores)
+
+    if daily_chore_assignments["Jaanuk"] is not None and daily_chore_assignments["Jaanuk"]["chores"] is not None:
+        available_chores -= set(daily_chore_assignments["Jaanuk"]["chores"])
+    if daily_chore_assignments["Peach"] is not None and daily_chore_assignments["Peach"]["chores"] is not None:
+        available_chores -= set(daily_chore_assignments["Peach"]["chores"])
+
     return random.sample(available_chores, 2)
+
 
 def update_daily_chore_assignments():
     today = datetime.now().date()
@@ -38,10 +45,32 @@ def update_daily_chore_assignments():
         
         daily_chore_assignments[person] = {"chores": choose_random_chores(person), "date": today}
 
+
 @app.route('/')
-def index():
+def choose_user():
+    return render_template('index.html')
+
+@app.route('/set_user', methods=['POST'])
+def set_user():
+    user = request.form.get('user')
+    if user in daily_chore_assignments:
+        return redirect(url_for('show_chores', user=user))
+    else:
+        return "Invalid user"
+
+@app.route('/chores/<user>')
+def show_chores(user):
     update_daily_chore_assignments()
-    return render_template('index.html', assignments=daily_chore_assignments, scores=completed_chores)
+
+    if user not in daily_chore_assignments:
+        return "Invalid user"
+
+    user_chores = daily_chore_assignments[user].get("chores")  # Use .get to handle None
+
+    if user_chores is None:
+        return "No chores assigned for today."
+
+    return render_template('chores.html', user=user, chores=user_chores)
 
 if __name__ == '__main__':
     app.run(debug=True)
