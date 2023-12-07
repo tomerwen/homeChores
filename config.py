1795762cdbc4299a74fb2380c0b7cb6c7eb80530
Jaanuk32 <tomerwen@users.noreply.github.com>
2023-12-07 14:57:38 +0200

    Added credentials.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    MONGO_URI = 'mongodb://mongodb-host:27017/home_chores'
-
-class TestConfig(Config):
-    MONGO_URI = 'mongodb://localhost:27017/test_database'
